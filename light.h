37f51673825794586381e3e0b3eccda8c0d7529f
David Feng <jiguanglizipao@163.com>
2016-12-26 19:24:30 +0800

    Merge

--- light.h
+++ light.h
@@ -2,7 +2,6 @@
 #define LIGHT_H
 
 #include "point.h"
-#include "kdtree.h"
 
 struct Color
 {
@@ -19,31 +18,10 @@ Color operator* (const double &k, const Color &x);
 double operator* (const Color &x, const Color &y);
 struct Light
 {
-    double ma[3], mi[3];
+    Point3D loc;
     Color col;
-    double check_aabb(Point3D view, Point3D ray)
-    {
-        double l = sqrt(ray*ray), s[3]={view.x, view.y, view.z}, v[3]={ray.x/l, ray.y/l, ray.z/l};
-        Point3D pos;
-        if (mi[0]<s[0]+eps && s[0]<ma[0]+eps && mi[1]<s[1]+eps && s[1]<ma[1]+eps && mi[2]<s[2]+eps && s[2]<ma[2]+eps)return 0;
-        double dis = 1e15;
-        for(int i=0;i<3;i++)
-        {
-            if (fabs(v[i]) > eps)
-            {
-                double t = ((v[i]>0?mi[i]:ma[i]) - s[i]) / v[i];
-                if (t > eps)
-                {
-                    pos = view + t * ray;
-                    double p[3]={pos.x, pos.y, pos.z};
-                    if (mi[(i+1)%3]<p[(i+1)%3]+eps && p[(i+1)%3]<ma[(i+1)%3]+eps && mi[(i+2)%3]<p[(i+2)%3]+eps && p[(i+2)%3]<ma[(i+2)%3]+eps)dis = std::min(dis, fabs(t));
-                }
-            }
-        }
-        return dis;
-    }
-    Light(Color _col = Color())
-        :col(_col)
+    Light(Point3D _loc = Point3D(), Color _col = Color())
+        :loc(_loc), col(_col)
     {
     }
 };
